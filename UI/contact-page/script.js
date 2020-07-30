7e203d5fcf01a8c863bdf7b7552fece7c1461668
Innocent Ingabire <inno100os@gmail.com>
2020-07-30 17:39:01 +0200

    can send data to database

--- UI/contact-page/script.js
+++ UI/contact-page/script.js
@@ -1,3 +1,20 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBL6YJVE6Pg6gABRWUL2g12iMDSxIld1EY",
+  authDomain: "innocent-ingabire---portfolio.firebaseapp.com",
+  databaseURL: "https://innocent-ingabire---portfolio.firebaseio.com",
+  projectId: "innocent-ingabire---portfolio",
+  storageBucket: "innocent-ingabire---portfolio.appspot.com",
+  messagingSenderId: "583842392187",
+  appId: "1:583842392187:web:2c9389562ce0f69a651ade",
+  measurementId: "G-4YZ43XFW66",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+var messagesRef = firebase.database().ref("messages");
+
 const form = document.getElementById("form");
 const email = document.getElementById("email");
 const name = document.getElementById("name");
@@ -50,11 +67,21 @@ function checkEmail(input) {
   }
 }
 
+function saveMessages(name, email, message) {
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    name: name,
+    email: email,
+    message: message,
+  });
+}
+
 // Event listener
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   checkRequired([name, email, message]);
-  checkLength(name, 3, 15);
+  checkLength(name, 3, 40);
   checkLength(message, 6, 1000);
   checkEmail(email);
+  saveMessages(name.value, email.value, message.value);
 });

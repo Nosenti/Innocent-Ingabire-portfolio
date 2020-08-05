8a8a2c9b09091d8c2f5f4b03577d31ef89c21e93
Innocent Ingabire <inno100os@gmail.com>
2020-08-05 07:44:13 +0200

    Validated blog posts and can post them to the database

--- UI/Admin-Manageblogs/script.js
+++ UI/Admin-Manageblogs/script.js
@@ -1,3 +1,18 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBL6YJVE6Pg6gABRWUL2g12iMDSxIld1EY",
+  authDomain: "innocent-ingabire---portfolio.firebaseapp.com",
+  databaseURL: "https://innocent-ingabire---portfolio.firebaseio.com",
+  projectId: "innocent-ingabire---portfolio",
+  storageBucket: "innocent-ingabire---portfolio.appspot.com",
+  messagingSenderId: "583842392187",
+  appId: "1:583842392187:web:2c9389562ce0f69a651ade",
+  measurementId: "G-4YZ43XFW66",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
@@ -32,7 +47,7 @@ function onFormSubmit() {
 function readFormData() {
   var formData = {};
   formData["title"] = document.getElementById("title").value;
-  // formData["messageBody"] = document.getElementById("message-body").value;
+  formData["message"] = document.getElementById("message-body").value;
 
   return formData;
 }
@@ -48,3 +63,84 @@ function insertNewRecord(data) {
   cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
                        <a onClick="onDelete(this)">Delete</a>`;
 }
+
+// Data validation in blog forms and firebase connection
+
+var blogsRef = firebase.database().ref("blogs");
+
+const form = document.getElementById("form");
+const title = document.getElementById("title");
+const message = document.getElementById("message-body");
+
+// Show input error message
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+}
+
+// check length
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be less than ${max} characters`
+    );
+  } else {
+    return true;
+  }
+}
+
+// Get fieldname
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+//check if email is valid
+function checkEmail(input) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    return true;
+  } else {
+    showError(input, "Email is not valid");
+  }
+}
+
+function saveBlogs(title, message) {
+  var newBlogRef = blogsRef.push();
+  newBlogRef.set({
+    title: title,
+    message: message,
+  });
+}
+// Elements validity
+
+// Event listener
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (
+    // checkRequired([name, email, message]) &&
+    checkLength(title, 3, 80) &&
+    checkLength(message, 6, 4000)
+  ) {
+    // save message to database
+    saveBlogs(title.value, message.value);
+
+    //display alert
+    document.querySelector(".alert").style.display = "block";
+
+    // Hide alert after 3 seconds
+    setTimeout(function () {
+      document.querySelector(".alert").style.display = "none";
+    }, 1500);
+
+    // Clear form
+    document.getElementById("form").reset();
+  }
+});
